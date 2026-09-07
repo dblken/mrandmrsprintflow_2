523b1ccdd2b3a3ac3b21cccfe81792f45a6fee57
arrontuazon60892-cyber <arrontuazon60892@gmail.com>
2026-09-07 22:04:07 +0800

    update portfolio

--- staff/customizations.php
+++ staff/customizations.php
@@ -686,21 +686,29 @@ $online_closed_count = 0;
             scrollbar-gutter: stable;
         }
         .customizations-data-table { min-width: 940px; table-layout: fixed; }
+        .customizations-data-table th,
+        .customizations-data-table td {
+            padding-left: 18px !important;
+            padding-right: 18px !important;
+        }
         .customizations-data-table .col-order,
         .customizations-data-table th:nth-child(1),
-        .customizations-data-table td:nth-child(1) { width: 16% !important; }
+        .customizations-data-table td:nth-child(1) { width: 15% !important; }
         .customizations-data-table .col-info,
         .customizations-data-table th:nth-child(2),
-        .customizations-data-table td:nth-child(2) { width: 24% !important; }
+        .customizations-data-table td:nth-child(2) { width: 22% !important; }
         .customizations-data-table .col-status,
         .customizations-data-table th:nth-child(3),
         .customizations-data-table td:nth-child(3) { width: 14% !important; }
         .customizations-data-table .col-customer,
         .customizations-data-table th:nth-child(4),
-        .customizations-data-table td:nth-child(4) { width: 15% !important; }
+        .customizations-data-table td:nth-child(4) { width: 17% !important; }
         .customizations-data-table .col-created,
         .customizations-data-table th:nth-child(5),
-        .customizations-data-table td:nth-child(5) { width: 18% !important; }
+        .customizations-data-table td:nth-child(5) {
+            width: 19% !important;
+            text-align: left !important;
+        }
         .customizations-data-table .col-action,
         .customizations-data-table th:nth-child(6),
         .customizations-data-table td:nth-child(6) { width: 13% !important; }
